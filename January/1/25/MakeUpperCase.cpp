c720f637870a81ee6448019988add35a2940899a
Mathew Bernard <92895007+thematbernard@users.noreply.github.com>
2023-01-25 16:29:29 -0500

    Create MakeUpperCase.cpp

---
+++ January/1/25/MakeUpperCase.cpp
@@ -0,0 +1,19 @@
+
+//Write a function which converts the input string to uppercase.
+
+//My solution
+#include <string>
+#include <cstring>
+
+std::string makeUpperCase (const std::string& input_str)
+{
+  std::string string;
+  
+  for( int index = 0; index < input_str.length(); index++)
+    string.push_back(input_str[index]);
+  
+  for(int i = 0; i < input_str.length(); i++)
+    string[i] = string[i] - 32;
+  
+  return string;
+}
